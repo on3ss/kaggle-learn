2ce4db8e51bba5c02ecb3c6b49dfda669e002d16
Oness Office WSL <auditoree9@gmail.com>
2024-03-08 17:02:59 +0530

    RandomForestGenerator and refactored predict

--- src/basic_prediction.py
+++ src/basic_prediction.py
@@ -1,13 +1,11 @@
 """Module for making basic predictions using Decision Tree Regressor."""
 
-from typing import Union
 import pandas as pd
-from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 
 
-def predict(data: pd.DataFrame, max_leaf_nodes: Union[int, None] = None):
+def predict(model, data: pd.DataFrame):
     """
     Trains a Decision Tree Regressor model on
     housing data and prints predictions for the first 5 houses.
@@ -24,19 +22,6 @@ def predict(data: pd.DataFrame, max_leaf_nodes: Union[int, None] = None):
     y = data.Price
 
     train_x, val_x, train_y, val_y = train_test_split(x, y, random_state=0)
-
-    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes)
     model.fit(train_x, train_y)
-    print("Predicted prices:")
     predictions = model.predict(val_x)
     return mean_absolute_error(val_y, predictions)
-
-
-def run_predictions(data: pd.DataFrame):
-    """
-    Run predictions for different value of max leaf nodes
-    """
-    for leaf_nodes in [None, 5, 50, 500, 5000]:
-        print(
-            f"Leaf Node: {leaf_nodes} | Mean Absolute Error: {predict(data, leaf_nodes)}"
-        )
--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
 import basic_prediction
 from utils import file_util
 
@@ -7,7 +9,20 @@ def main():
     melbourne_data_file_path = file_util.file_path("datasets", "melb_data.csv")
     melbourne_data = pd.read_csv(melbourne_data_file_path)
 
-    basic_prediction.run_predictions(melbourne_data)
+    max_leaf_nodes_list = [None, 5, 50, 500, 5000]
+
+    print("DecisionTreeRegressor")
+    for max_leaf_nodes in max_leaf_nodes_list:
+        mae = basic_prediction.predict(
+            DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes), melbourne_data
+        )
+        print(f"Max Leaf Nodes: {max_leaf_nodes} \t MAE: {mae}")
+
+    print("\n")
+
+    print("RandomForestRegressor")
+    mae = basic_prediction.predict(RandomForestRegressor(), melbourne_data)
+    print(f"MAE: {mae}")
 
 
 if __name__ == "__main__":
